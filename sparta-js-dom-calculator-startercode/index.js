beab8328fb8f38d6d74f7f73a9b3b86936302c20
NaO <noukongka@spartaglobal.com>
2018-08-09 18:11:23 +0100

    Add file

---
+++ sparta-js-dom-calculator-startercode/index.js
@@ -0,0 +1,6 @@
+var numButton = getElementsByClassName('buttonNum');
+var operButton = getElementsByClassName('operato');
+var equalButton = getElementsByClassName('equals');
+var clearButton = getElementsByClassName('clear');
+
+numberButton.addEventListener()
